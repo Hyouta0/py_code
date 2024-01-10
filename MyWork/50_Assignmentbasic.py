a12fd9710bcfb0f98e2ad8b8b5b59b3d706e5684
kiran biradar <kiranbiradar2000@gmail.com>
2024-01-10 07:04:55 +0000

    updated emp data generater \n ass 50 ongoing

--- MyWork/50_Assignmentbasic.py
+++ MyWork/50_Assignmentbasic.py
@@ -1,4 +1,14 @@
 # write a program to compute fibonaci number of given number(recurssion);
 
+# 1,2,3,5;
 
+num1 =1;
+num2 =1;
 
+cnt =0;
+while(cnt <4):
+    cnt+=1;
+    tmp = num2;
+    num2 =num1;
+    num1 = tmp+num2;
+print(num2);
--- MyWork_high/emp_data_gen.py
+++ MyWork_high/emp_data_gen.py
@@ -1,25 +1,29 @@
 # emp_id , first name ,last name ,age , hight, salary , manager id
 import random
 
+
 f_nameList=["kiran","nishad","harshal","sanket"];
 l_nameList=["biradar","deshpande","bhandalkar","raskar"];
-age=["5.11","5.5","5.9","6.0","6.5","6.9","7.0"];
-empRec=[];
 
-cnt=1;
-for i in range(20):
-    rndList = [cnt,random.choice(f_nameList),random.choice(l_nameList),random.randint(20,50),random.choice(age),random.randint(25000,600000),random.randint(1,cnt)];
-    empRec.append(rndList);
-    cnt+=1;
 
 path = r"./EmpData.csv";
-fp = open(path,'a');
 hdr = "emp_id,first_name,last_name,age,hight,salary,manager_id\n";
-fp.write(hdr);
+id =0;
 
-for emp in empRec:
-    empLine = str(emp[0])+","+emp[1]+","+emp[2]+","+str(emp[3])+","+emp[4]+","+str(emp[5])+","+str(emp[6])+"\n";
-    fp.write(empLine);
+fp = open(path,'a');
+fp.write(hdr);
+cnt = int(input("Enter no of data to be generated : "));
+
+for i in range(cnt):
+    id+=1;
+    line = str(id)+","+\
+            random.choice(f_nameList)+","+\
+            random.choice(l_nameList)+","+\
+            str(random.randint(20,50))+","+\
+            str(round(random.uniform(5.0,7.0),2))+","+\
+            str(random.randint(20000,100000))+","+\
+            str(random.randint(1,id))+"\n";
+    fp.write(line);
 
 fp.close();
 print("Done");
