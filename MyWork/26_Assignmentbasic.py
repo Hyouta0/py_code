a8c79f273a28fbac0df5ea4a54bc3811c486ac78
kiran biradar <kiranbiradar2000@gmail.com>
2024-01-19 06:21:53 +0000

    26th assignment completed

---
+++ MyWork/26_Assignmentbasic.py
@@ -0,0 +1,12 @@
+'''
+"26","print patterns -- madhura to fill this",
+"26_Assignmentloops","loops","","","","","","","","","",""
+****
+***
+**
+*
+'''
+for i in range(4):
+    for j in range(4-i,0,-1):
+        print("*",end="")
+    print()
