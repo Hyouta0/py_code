668f9470eb03c86e1ba2d47d776ea40834656c12
kiran biradar <kiranbiradar2000@gmail.com>
2024-01-30 17:41:18 +0530

    playing with pandas

--- Praticals/pandas/pandasWithCsv.py
+++ Praticals/pandas/pandasWithCsv.py
@@ -4,9 +4,18 @@ path = r'./hwaData.csv'
 df = pd.read_csv(path,\
                  names=['Count','Name','Height','Weight','Age'],skiprows=[0])
 
-#print(df[df['Age'] < 41].describe())
+#Slicing by columns
+print(df.Name)
+print(df['Age'])
+print(df[['Name','Age','Weight','Height']])
+#slicing row by index location
+print(df.iloc[2,3])#second row thired column 
+print("printing row : \n",df.iloc[2,])#second row
+print("printing colun: \n",df.iloc[:,2])#print all second colunm
+print(df.iloc[2:7])#print rows from 2 to 6
+print(df.loc[1:7])#print start from 1 not 0 both inclusive
 
-df1 = df[df['Age'] == 40]
 
-print(df1)
+
+
 
