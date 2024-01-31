f56a78d00ca55b3fb34e122c64a09efbaaab5cdf
kiran biradar <kiranbiradar2000@gmail.com>
2024-01-31 17:24:42 +0530

    59 completed

---
+++ Assignments/59_Assignmentbasic.py
@@ -0,0 +1,9 @@
+'''
+use same hard coded list as given in assignment 3,
+find total of all numbers in the list
+'''
+num = [23,11,42,45,54,7,2,34,89,9]
+sum = 0
+for i in num:
+    sum+=i 
+print(sum)
